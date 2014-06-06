dfb6af2a0c7a9819097bb94cae3aca68e142ad79
Tim Smith <code@abznak.com>
2014-06-06 21:24:07 +1000

    go

--- turtle/sphere.lua
+++ turtle/sphere.lua
@@ -12,6 +12,93 @@ tx = 0
 ty = 0
 tz = 0
 
+cubesize = 3
+
+mcubex = 2
+mcubey = 2
+mcubez = 2
+
+--[[
+  pre - in bottom left back of cube
+  post - in bottom left back of cube above
+  places a block at a random point in the cube
+]]
+function rndInCube()
+	dbg("rndInCube")
+	local dx = math.random(cubesize-1)
+	local dy = math.random(cubesize-1)
+	local dz = math.random(cubesize-1)
+	movex(dx)
+	movey(dy)
+	movez(dz)
+	select_building_material()
+	dbg("placed")
+	turtle.placeDown()
+	movex(-dx)
+	movey(-dy)
+	movez(cubesize-dz)
+	dbg("/rndInCube")
+end
+
+function makeCubes()
+	dbg('makeCubes')
+	for x = 1, mcubex, 1 do
+		for y = 1, mcubey, 1 do
+			for z = 1, mcubez, 1 do
+				rndInCube()
+			end
+			movey(cubesize)
+			movez(mcubez * cubesize)
+		end
+		movex(cubesize)
+		movey(mcubey * cubesize)
+	end
+	movex(mcubez * cubesize)
+end				
+	
+	
+
+
+
+
+function movez(n)
+	if n == 0 then
+		return
+	end
+	absn = math.abs(n)
+	unitn = n/absn
+	for i = 1, math.abs(n), 1 do
+		if (n < 0) then
+			moved = turtle.down()
+		else
+			moved = turtle.up()
+		end
+		if not moved then
+			dbg('obstruction')
+		else
+			tz = tz + unitn
+		end
+	end
+end
+function movey(n)
+	if n == 0 then
+		return
+	end
+	absn = math.abs(n)
+	unitn = n/absn
+	for i = 1, math.abs(n), 1 do
+		if (n < 0) then
+			moved = turtle.back()
+		else
+			moved = turtle.forward()
+		end
+		if not moved then
+			dbg('obstruction')
+		else
+			ty = ty + unitn
+		end
+	end
+end
 function movex(n)
 	if n == 0 then
 		return
@@ -96,6 +183,7 @@ function select_building_material()
   return turtle.getItemCount(ss) > 0
 end
    
-   
 
-movex(3)
+   
+movey(20)
+movex(15)
